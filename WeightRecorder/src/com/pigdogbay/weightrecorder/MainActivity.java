cfc7865dcbcbf72e3c0d281ce40479b6719cfa2d
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-05-19 10:31:02 +0000

    Added export/import on main menu

--- WeightRecorder/src/com/pigdogbay/weightrecorder/MainActivity.java
+++ WeightRecorder/src/com/pigdogbay/weightrecorder/MainActivity.java
@@ -117,16 +117,12 @@ public class MainActivity extends Activity implements
 			Intent intent = new Intent(this, AboutActivity.class);
 			startActivity(intent);
 			break;
-//		case (R.id.menu_create_500_readings):
-//			List<Reading> dummy = DummyData.CreateReadings(500);
-//			MainModel.getDatabase().deleteAllReadings();
-//			MainModel.getDatabase().addReadings(dummy);
-//			break;
-//		case (R.id.menu_my_readings):
-//			List<Reading> myReadings = DummyData.GetReadings();
-//			MainModel.getDatabase().deleteAllReadings();
-//			MainModel.getDatabase().addReadings(myReadings);
-//			break;
+		case (R.id.menu_main_export):
+			ActivitiesHelper.startExportActivity(this);
+			break;
+		case (R.id.menu_main_import):
+			ActivitiesHelper.startImportActivity(this);
+			break;
 		default:
 			return false;
 		}
