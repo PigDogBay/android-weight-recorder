f20faf3b2389671d50a0bed8fb6b3d620b08a773
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-06-13 20:59:27 +0000

    Completed UI report code

--- WeightRecorder/src/com/pigdogbay/weightrecorder/ReportActivity.java
+++ WeightRecorder/src/com/pigdogbay/weightrecorder/ReportActivity.java
@@ -3,88 +3,103 @@ package com.pigdogbay.weightrecorder;
 import java.util.Date;
 import java.util.List;
 
-import com.pigdogbay.weightrecorder.model.BMICalculator;
+import com.pigdogbay.androidutils.utils.ActivityUtils;
 import com.pigdogbay.weightrecorder.model.DummyData;
 import com.pigdogbay.weightrecorder.model.MainModel;
 import com.pigdogbay.weightrecorder.model.Query;
 import com.pigdogbay.weightrecorder.model.Reading;
 import com.pigdogbay.weightrecorder.model.ReportAnalysis;
 import com.pigdogbay.weightrecorder.model.ReportText;
-import com.pigdogbay.weightrecorder.model.TrendAnalysis;
 import com.pigdogbay.weightrecorder.model.UserSettings;
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.text.format.DateUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class ReportActivity extends Activity {
-	private static final int MINIMUM_READINGS = 1;
-	public static final long DAY_IN_MILLIS = 24L * 60L * 60L * 1000L;
+	private static final int MINIMUM_READINGS = 2;
 	private UserSettings _UserSettings;
-	private MainModel _MainModel;
 	private ReportText _ReportText;
 	private ReportAnalysis _ReportAnalysis;
-	
-	
+	private boolean isLoaded = false;
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report);
-		_MainModel = new MainModel(this);
-		_UserSettings = _MainModel.getUserSettings();
-		List<Reading> readings = _MainModel.getDatabase().getAllReadings();
-		if (readings.size() < MINIMUM_READINGS) {
-			readings = DummyData.createRandomData(120);
-			ActivitiesHelper.showInfoDialog(this, R.string.report_notenoughdata_title, R.string.report_notenoughdata_message);
+		if (!isLoaded) {
+			MainModel mainModel = new MainModel(this);
+			_UserSettings = mainModel.getUserSettings();
+			List<Reading> readings = mainModel.getDatabase().getAllReadings();
+			if (readings.size() < MINIMUM_READINGS) {
+				readings = DummyData.createRandomData(120);
+				ActivitiesHelper.showInfoDialog(this,
+						R.string.report_notenoughdata_title,
+						R.string.report_notenoughdata_message);
+			}
+			Query query = new Query(readings);
+			_ReportAnalysis = new ReportAnalysis(_UserSettings, query);
+			_ReportText = new ReportText(this, _UserSettings);
+			mainModel.close();
+			isLoaded = true;
 		}
-		_ReportAnalysis = new ReportAnalysis(_UserSettings, readings);
-		_ReportText = new ReportText(this, _UserSettings);
 		populateTextViews();
-//		TextView textView = (TextView) findViewById(R.id.ReportText);
-//		Query query = new Query(readings);
-//		textView.setText(Html.fromHtml(createReport(query)));
-//		textView.setMovementMethod(new ScrollingMovementMethod());
-	}
-	
-	private void populateTextViews()
-	{
-		TextView textView = (TextView) findViewById(R.id.ReportTextViewLatestBMI);
-		textView.setText(_ReportText.bmiToString(_ReportAnalysis.getLatestBMI()));
-		textView = (TextView) findViewById(R.id.ReportTextViewGoalBMI);
-		textView.setText(_ReportText.bmiToString(_ReportAnalysis.getTargetBMI()));
-		textView = (TextView) findViewById(R.id.ReportTextViewIdealRange);
-		textView.setText(_ReportText.idealWeightRangeToString(
-				_ReportAnalysis.getBottomOfIdealWeightRange(),
-				_ReportAnalysis.getTopOfIdealWeightRange()));
-		textView = (TextView) findViewById(R.id.ReportTextViewMinWeight);
-		textView.setText(_ReportText.weightToString(_ReportAnalysis.getMinWeight()));
-		textView = (TextView) findViewById(R.id.ReportTextViewMaxWeight);
-		textView.setText(_ReportText.weightToString(_ReportAnalysis.getMaxWeight()));
-		textView = (TextView) findViewById(R.id.ReportTextViewAverageWeight);
-		textView.setText(_ReportText.weightToString(_ReportAnalysis.getAverageWeight()));
-		textView = (TextView) findViewById(R.id.ReportTextViewAverageBMI);
-		textView.setText(_ReportText.bmiToString(_ReportAnalysis.getAverageBMI()));
-		textView = (TextView) findViewById(R.id.ReportTextViewCount);
-		textView.setText(_ReportText.weightToString(_ReportAnalysis.getCount()));
-	}
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		_MainModel.close();
 	}
 
-	private String getTabbing() {
-		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
-			return " ";
-		}
-		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
-			return "\t\t";
-		}
-		return "\t\t\t\t";
+	private void populateTextViews() {
+		TextView textView = (TextView) findViewById(R.id.ReportTextViewLatestBMI);
+		textView.setText(_ReportText.getBMIString(_ReportAnalysis.getLatestBMI()));
+		textView = (TextView) findViewById(R.id.ReportTextViewGoalBMI);
+		textView.setText(_ReportText.getBMIString(_ReportAnalysis.getTargetBMI()));
+		textView = (TextView) findViewById(R.id.ReportTextViewIdealRange);
+		textView.setText(_ReportText.getIdealWeightRange(
+				_ReportAnalysis.getBottomOfIdealWeightRange(),
+				_ReportAnalysis.getTopOfIdealWeightRange()));
+
+		textView = (TextView) findViewById(R.id.ReportTextViewMinWeight);
+		textView.setText(_ReportText.getWeightString(_ReportAnalysis.MinWeight));
+		textView = (TextView) findViewById(R.id.ReportTextViewMaxWeight);
+		textView.setText(_ReportText.getWeightString(_ReportAnalysis.MaxWeight));
+		textView = (TextView) findViewById(R.id.ReportTextViewAverageWeight);
+		textView.setText(_ReportText
+				.getWeightString(_ReportAnalysis.AverageWeight));
+		textView = (TextView) findViewById(R.id.ReportTextViewAverageBMI);
+		textView.setText(_ReportText.getBMIString(_ReportAnalysis
+				.getAverageBMI()));
+		textView = (TextView) findViewById(R.id.ReportTextViewCount);
+		textView.setText(Integer.toString(_ReportAnalysis.Count));
+
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendAllGoalDate);
+		textView.setText(_ReportText.getDateString(_ReportAnalysis
+				.getEstimatedDateUsingAllTime()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendAllValue);
+		textView.setText(_ReportText.getWeightTrend(_ReportAnalysis
+				.getWeeklyTrendAllTime()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendAllTitle);
+		textView.setText(_ReportText.getWeightTrendDirection(_ReportAnalysis
+				.getWeeklyTrendAllTime()));
+
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendMonthGoalDate);
+		textView.setText(_ReportText.getDateString(_ReportAnalysis
+				.getEstimatedDateUsingLastMonth()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendMonthValue);
+		textView.setText(_ReportText.getWeightTrend(_ReportAnalysis
+				.getWeeklyTrendOverLastMonth()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendMonthTitle);
+		textView.setText(_ReportText.getWeightTrendDirection(_ReportAnalysis
+				.getWeeklyTrendOverLastMonth()));
+
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendWeekGoalDate);
+		textView.setText(_ReportText.getDateString(_ReportAnalysis
+				.getEstimatedDateUsingLastWeek()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendWeekValue);
+		textView.setText(_ReportText.getWeightTrend(_ReportAnalysis
+				.getWeeklyTrendOverLastWeek()));
+		textView = (TextView) findViewById(R.id.ReportTextViewTrendWeekTitle);
+		textView.setText(_ReportText.getWeightTrendDirection(_ReportAnalysis
+				.getWeeklyTrendOverLastWeek()));
+
 	}
 
 	@Override
@@ -95,8 +110,7 @@ public class ReportActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId())
-		{
+		switch (item.getItemId()) {
 		case (R.id.menu_report_home):
 			finish();
 			break;
@@ -109,139 +123,11 @@ public class ReportActivity extends Activity {
 		return true;
 	}
 
-	private String createReport(Query query) {
-		Date now = new Date();
-		Date lastWeek = new Date(now.getTime() - 7L * DAY_IN_MILLIS);
-		Date last30Days = new Date(now.getTime() - 30L * DAY_IN_MILLIS);
-		StringBuilder builder = new StringBuilder();
-		builder.append("<b>Body Mass Index</b><br/><br/>");
-		builder.append(createBMI(query));
-		builder.append("<br/><br/><b>Last 7 Days</b><br/><br/>");
-		builder.append(createStats(query.getReadingsBetweenDates(lastWeek, now)));
-		builder.append("<br/><br/><b>Last 30 Days</b><br/><br/>");
-		builder.append(createStats(query.getReadingsBetweenDates(last30Days,
-				now)));
-		builder.append("<br/><br/><b>All Time</b><br/><br/>");
-		builder.append(createStats(query));
-		return builder.toString();
-
-	}
-
-	private String createBMI(Query query) {
-		BMICalculator bmiCalculator = new BMICalculator(_UserSettings);
-		StringBuilder builder = new StringBuilder();
-		Reading latest = query.getLatestReading();
-		double bmi = bmiCalculator.calculateBMI(latest);
-		builder.append("BMI\t");
-		builder.append(getTabbing());
-		builder.append(String.format("%.1f", bmi));
-		builder.append("<br/>Class");
-		builder.append(getTabbing());
-		builder.append(getBMIClass(bmi));
-		builder.append("<br>Goal\t");
-		builder.append(getTabbing());
-		bmi = bmiCalculator.calculateBMI(_UserSettings.TargetWeight);
-		builder.append(weightToString(_UserSettings.TargetWeight));
-		builder.append(String.format(" (BMI %.1f)", bmi));
-		builder.append("<br/><br/>Ideal Weight Range<br/>");
-		double startRange = bmiCalculator.calculateWeightFromBMI(BMICalculator.UNDERWEIGHT_UPPER_LIMIT);
-		double endRange = bmiCalculator.calculateWeightFromBMI(BMICalculator.NORMAL_UPPER_LIMIT);
-		builder.append(weightToString(startRange) + "  -  "
-				+ weightToString(endRange));
-		return builder.toString();
-	}
-
-	private String createStats(Query query) {
-		BMICalculator bmiCalculator = new BMICalculator(_UserSettings);
-		if (query.getReadings().size() == 0) {
-			return "No readings available";
-		}
-		Reading reading = query.getMinWeight();
-		StringBuilder builder = new StringBuilder();
-		builder.append("Min weight");
-		builder.append(getTabbing());
-		builder.append(weightToString(reading.getWeight()));
-		reading = query.getMaxWeight();
-		builder.append("<br/>Max weight");
-		builder.append(getTabbing());
-		builder.append(weightToString(reading.getWeight()));
-		builder.append("<br/>Average\t\t");
-		builder.append(getTabbing());
-		double avg = query.getAverageWeight();
-		builder.append(weightToString(avg));
-		builder.append("<br/>Average BMI");
-		builder.append(getTabbing());
-		builder.append(String.format("%.1f", bmiCalculator.calculateBMI(avg)));
-		builder.append("<br/>Trend\t\t\t");
-		builder.append(getTabbing());
-		TrendAnalysis trendAnalysis = new TrendAnalysis(query.getReadings());
-		double trend = trendAnalysis.getTrendInDays();
-		String prefix = "";
-//		if (trend != 0D) {
-//			prefix = trend > 0 ? getString(R.string.report_positive_trend)
-//					: getString(R.string.report_negative_trend);
-//			prefix = prefix + " ";
-//		}
-		trend = Math.abs(trend);
-		builder.append(prefix);
-		builder.append(weightToString(trend) + " "
-				+ getString(R.string.report_per_week));
-		try {
-			long estimatedGoalDateMillis = trendAnalysis.getEstimatedDate(_UserSettings.TargetWeight);
-			if (TrendAnalysis.isGoalDateValid(estimatedGoalDateMillis)) {
-				builder.append("<br/>Goal Date\t");
-				builder.append(getTabbing());
-				String dateText = DateUtils.formatDateTime(this,
-						estimatedGoalDateMillis, DateUtils.FORMAT_SHOW_DATE
-								| DateUtils.FORMAT_SHOW_YEAR);
-				builder.append(dateText);
-			}
-		}
-		catch (Exception e) {
-		}
-		return builder.toString();
-
-	}
-	/**
-	 * @param weight in kilograms
-	 * @return string representation with user units
-	 */
-	private String weightToString(double weight) {
-		weight = _UserSettings.WeightConverter.convert(weight);
-		return _UserSettings.WeightConverter.getDisplayString(weight);
-	}
-
 	private void emailReport() {
 		Date now = new Date();
-		String dateText = DateUtils.formatDateTime(this, now.getTime(),
-				DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR);
+		String dateText = _ReportText.getDateString(now.getTime());
 		String subject = getString(R.string.report_email_subject) + " "
 				+ dateText;
-//		TextView textView = (TextView) findViewById(R.id.ReportText);
-//		ActivityUtils.SendEmail(this, null, subject, textView.getText()
-//				.toString());
+		ActivityUtils.SendEmail(this, null, subject, _ReportText.createReport(_ReportAnalysis));
 	}
-
-	private String getBMIClass(double bmi) {
-		if (bmi < BMICalculator.SEVERLY_UNDERWEIGHT_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_severely_underweight);
-		}
-		else if (bmi < BMICalculator.UNDERWEIGHT_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_underweight);
-		}
-		else if (bmi < BMICalculator.NORMAL_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_normal);
-		}
-		else if (bmi < BMICalculator.OVERWEIGHT_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_overweight);
-		}
-		else if (bmi < BMICalculator.OBESE1_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_obese1);
-		}
-		else if (bmi < BMICalculator.OBESE2_UPPER_LIMIT) {
-			return getString(R.string.bmi_class_obese2);
-		}
-		return getString(R.string.bmi_class_obese3);
-	}
-
 }
