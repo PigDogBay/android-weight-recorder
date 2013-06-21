026a7d688bd810ca6a9888c8d789f7e1d728981f
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-06-21 10:21:48 +0000

    Improved unit conversion accuracy

--- trunk/WeightRecorderUnitTests/src/com/pigdogbay/weightrecorder/test/Utils.java
+++ trunk/WeightRecorderUnitTests/src/com/pigdogbay/weightrecorder/test/Utils.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 
 public class Utils {
 	public static final long DAY_IN_MILLIS = 24L * 60L * 60L * 1000L;
+	public static final long YEAR_IN_MILLIS = DAY_IN_MILLIS*365L;
 
 	public static void assertRounded(double expected, double actual)
 	{
