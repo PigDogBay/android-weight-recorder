38431a097ff2cc8e951a549d4b8ac1f2a4bc94df
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-07-10 17:00:19 +0000

    Improved report sharing, menu shows icon. Now shared as plain text.

--- trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ReportActivity.java
+++ trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ReportActivity.java
@@ -3,7 +3,6 @@ package com.pigdogbay.weightrecorder;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import com.pigdogbay.androidutils.utils.ActivityUtils;
 import com.pigdogbay.weightrecorder.model.DummyData;
 import com.pigdogbay.weightrecorder.model.MainModel;
 import com.pigdogbay.weightrecorder.model.Query;
@@ -27,7 +26,7 @@ public class ReportActivity extends Activity {
 	private ReportText _ReportText;
 	private ReportFormatting _ReportFormatting;
 	private boolean isLoaded = false;
-	
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report);
@@ -35,16 +34,16 @@ public class ReportActivity extends Activity {
 			createReport();
 			isLoaded = true;
 		}
-		try
-		{
+		try {
 			populateTextViews();
 		}
-		catch(Exception e){
-			Log.v(MainActivity.TAG, "Unable to populate views: "+e.getMessage());
+		catch (Exception e) {
+			Log.v(MainActivity.TAG,
+					"Unable to populate views: " + e.getMessage());
 		}
 	}
-	private void createReport()
-	{
+
+	private void createReport() {
 		MainModel mainModel = new MainModel(this);
 		UserSettings userSettings = mainModel.getUserSettings();
 		List<Reading> readings = mainModel.getDatabase().getAllReadings();
@@ -59,17 +58,17 @@ public class ReportActivity extends Activity {
 		_ReportFormatting = new ReportFormatting(this, userSettings);
 		_ReportText = new ReportText(analysis, _ReportFormatting);
 		mainModel.close();
-		
+
 	}
 
-	private void populateTextViews() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException {
-		for (Map.Entry<String, String> entry : _ReportText.getEntrySet())
-		{
+	private void populateTextViews() throws IllegalArgumentException,
+			IllegalAccessException, NoSuchFieldException {
+		for (Map.Entry<String, String> entry : _ReportText.getEntrySet()) {
 			String fieldName = entry.getKey().replace("$", TEXT_FIELD_PREFIX);
 			int id = R.id.class.getField(fieldName).getInt(null);
-			TextView textView = (TextView)findViewById(id);
+			TextView textView = (TextView) findViewById(id);
 			textView.setText(entry.getValue());
-		}	
+		}
 	}
 
 	@Override
@@ -80,7 +79,8 @@ public class ReportActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+		switch (item.getItemId())
+		{
 		case (R.id.menu_report_home):
 			finish();
 			break;
@@ -94,11 +94,11 @@ public class ReportActivity extends Activity {
 	}
 
 	private void emailReport() {
-		String template = Html.fromHtml(this.getString(R.string.report_template)).toString();
 		Date now = new Date();
 		String dateText = _ReportFormatting.getDateString(now.getTime());
-		String subject = getString(R.string.report_email_subject) + " "
-				+ dateText;
-		ActivityUtils.SendEmail(this, null, subject, _ReportText.createReport(template));
+		String subject = getString(R.string.report_email_subject) + " "	+ dateText;
+		String template = Html.fromHtml(this.getString(R.string.report_template)).toString();
+		String text = _ReportText.createReport(template);
+		ActivitiesHelper.shareText(this, subject, text);
 	}
 }
