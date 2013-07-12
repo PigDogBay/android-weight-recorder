363984cad88e9504dce631597b43763fa65da8a8
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-07-12 14:10:23 +0000

    Added load backup readings feature

--- trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ImportActivity.java
+++ trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ImportActivity.java
@@ -1,5 +1,6 @@
 package com.pigdogbay.weightrecorder;
 
+import java.io.File;
 import java.util.List;
 
 import com.pigdogbay.androidutils.utils.FileUtils;
@@ -15,7 +16,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -23,110 +23,113 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ImportActivity extends Activity
-{
+public class ImportActivity extends Activity {
 	public static final String NEW_IMPORTED_READINGS = "com.pigdogbay.weightrecorder.NEW_IMPORTED_READINGS";
+
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_import);
 		((Button) findViewById(R.id.ImportOKButton))
-				.setOnClickListener(new OnClickListener()
-				{
-					public void onClick(View v)
-					{
+				.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
 						importReadings();
 					}
 				});
-	    Intent intent = getIntent();
-	    Uri uri = intent.getData();
-	    String action = intent.getAction();
-	    String type = intent.getType();
-	    log("Action: "+ action);
-	    log("Type: "+type);
-	    if (null!=uri)
-	    {
-	    	loadReadings(uri);
-	    }
-	    else
-	    {
-	    	showHelp();
-	    }
-	}
-	void log(String s)
-	{
-		s = s==null ? "null" : s;
-		Log.v("WR-Import",s);
-	}
-	private void loadReadings(Uri uri){
-	    try
-	    {
-	    	String path = uri.getPath();
-	    	log("Path: "+path);
-	    	String data = FileUtils.readText(this, uri);
-	    	TextView textView = (TextView)findViewById(R.id.ImportEdit);
-	    	textView.setText(data);
-	    }catch(Exception e)
-	    {
-	    	log(e.getMessage());
-	    }		
+		((Button) findViewById(R.id.ImportLoadBackupButton))
+				.setOnClickListener(new OnClickListener() {
+					public void onClick(View v) {
+						loadBackupReadings();
+					}
+				});
+		Intent intent = getIntent();
+		Uri uri = intent.getData();
+		if (null != uri) {
+			loadReadings(uri);
+		}
+		else {
+			showHelp();
+		}
 	}
 
-	private void showHelp()
-	{
+	private void loadReadings(Uri uri) {
+		try {
+			String data = FileUtils.readText(this, uri);
+			TextView textView = (TextView) findViewById(R.id.ImportEdit);
+			textView.setText(data);
+		}
+		catch (Exception e) {
+		}
+	}
+
+	private void loadBackupReadings() {
+		File latest = ActivitiesHelper.getLatestBackupFile(this);
+		if (latest == null) {
+			Toast.makeText(this, getString(R.string.import_no_backup_file),
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
+		try {
+			String data = FileUtils.readText(latest);
+			TextView textView = (TextView) findViewById(R.id.ImportEdit);
+			textView.setText(data);
+		}
+		catch (Exception e) {
+			Toast.makeText(this, getString(R.string.import_error_loading_backup_file),
+					Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private void showHelp() {
 		String title = getResources().getString(R.string.import_dialog_title);
-		String message = getResources().getString(R.string.import_dialog_message);
+		String message = getResources().getString(
+				R.string.import_dialog_message);
 		new AlertDialog.Builder(this)
 				.setIcon(android.R.drawable.ic_dialog_info)
 				.setTitle(title)
 				.setMessage(message)
 				.setPositiveButton(R.string.ok,
-						new DialogInterface.OnClickListener()
-						{
+						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
-									int which)
-							{
+									int which) {
 								dialog.dismiss();
 							}
 						}).show();
-		
+
 	}
-	
-	private void importReadings()
-	{
+
+	private void importReadings() {
 		EditText editText = (EditText) findViewById(R.id.ImportEdit);
 		String text = editText.getText().toString();
-		if (text == null || "".equals(text))
-		{
+		if (text == null || "".equals(text)) {
 			Toast.makeText(this, getString(R.string.import_no_text),
 					Toast.LENGTH_SHORT).show();
 		}
-		else
-		{
+		else {
 			List<Reading> readings = ReadingsSerializer.parse(text);
 			int count = readings.size();
-			if (count>0)
-			{
+			if (count > 0) {
 				MainModel mainModel = new MainModel(this);
-				try{
-					Synchronization sync = new Synchronization(mainModel.getReverseOrderedReadings());
+				try {
+					Synchronization sync = new Synchronization(
+							mainModel.getReverseOrderedReadings());
 					sync.Merge(readings);
 					mainModel.getDatabase().mergeReadings(sync._Readings);
 				}
-				finally{
+				finally {
 					mainModel.close();
-					}
-				
+				}
+
 				setResult(RESULT_OK);
 				Intent intent = new Intent(NEW_IMPORTED_READINGS);
 				LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
 			}
-			Toast.makeText(this, String.valueOf(count)+ getString(R.string.import_readings_added),
+			Toast.makeText(
+					this,
+					String.valueOf(count)
+							+ getString(R.string.import_readings_added),
 					Toast.LENGTH_SHORT).show();
 		}
 		this.finish();
 	}
-
-
 }
