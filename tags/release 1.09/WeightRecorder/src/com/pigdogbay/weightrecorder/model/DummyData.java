3cee2da359284c58e781bcb07ee25f32fed80356
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-07-18 13:35:35 +0000

    Release 1.09, 18th Jul 2013

---
+++ tags/release 1.09/WeightRecorder/src/com/pigdogbay/weightrecorder/model/DummyData.java
@@ -0,0 +1,67 @@
+package com.pigdogbay.weightrecorder.model;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+
+public class DummyData
+{
+	public static List<Reading> GetReadings()
+	{
+		List<Reading> readings = new ArrayList<Reading>();
+		readings.add(new Reading(95.5f,new Date(112,Calendar.DECEMBER,2,8,5),""));
+		readings.add(new Reading(95.8f,new Date(112,Calendar.DECEMBER,3,8,5),""));
+		readings.add(new Reading(95.3f,new Date(112,Calendar.DECEMBER,5,8,5),""));
+		readings.add(new Reading(94.8f,new Date(112,Calendar.DECEMBER,6,8,5),""));
+		readings.add(new Reading(95.1f,new Date(112,Calendar.DECEMBER,9,8,5),""));
+		readings.add(new Reading(96.4f,new Date(112,Calendar.DECEMBER,10,8,5),""));
+		readings.add(new Reading(94.4f,new Date(112,Calendar.DECEMBER,11,8,5),""));
+		readings.add(new Reading(95.1f,new Date(112,Calendar.DECEMBER,12,8,5),""));
+		readings.add(new Reading(95.3f,new Date(112,Calendar.DECEMBER,16,8,5),""));
+		readings.add(new Reading(95.0f,new Date(112,Calendar.DECEMBER,17,8,5),""));
+		readings.add(new Reading(94.9f,new Date(112,Calendar.DECEMBER,18,8,5),""));
+		readings.add(new Reading(94.0f,new Date(112,Calendar.DECEMBER,19,8,5),""));
+		readings.add(new Reading(94.5f,new Date(112,Calendar.DECEMBER,21,11,0),""));
+		readings.add(new Reading(94.6f,new Date(112,Calendar.DECEMBER,22,11,0),""));
+		readings.add(new Reading(93.7f,new Date(112,Calendar.DECEMBER,23,11,0),"pigged out on hot dogs after the pub"));
+		readings.add(new Reading(94.8f,new Date(112,Calendar.DECEMBER,24,11,0),""));
+		readings.add(new Reading(94.4f,new Date(112,Calendar.DECEMBER,26,11,0),""));
+		readings.add(new Reading(94.7f,new Date(112,Calendar.DECEMBER,28,11,0),""));
+		return readings;
+	}
+	
+	public static List<Reading> CreateReadings(int number)
+	{
+		List<Reading> readings = new ArrayList<Reading>();
+		Date date = new Date();
+		double weight = 70f;
+		
+		for (int i=0; i<number;i++)
+		{
+			readings.add(new Reading(weight,date,""));
+			weight = weight +0.05f;
+			date = new Date(date.getTime()-24*60*60*1000);
+		}
+		return readings;
+	}
+	
+	public static List<Reading> createRandomData(int period)
+	{
+		List<Reading> readings = new ArrayList<Reading>();
+		Random rand = new Random(42L);
+		Date today = new Date();
+		for (int i = period; i>=0 ;i--)
+		{
+			Date date = new Date(today.getTime()-24L*60L*60L*1000L*((long)i));
+			double weight = 80f+((double)i)*0.15f-(rand.nextDouble()-1f)/2;
+			readings.add(new Reading(weight,date,""));
+		}
+		
+		return readings;
+		
+	}
+} 
+
---
+++ tags/release 1.09/WeightRecorder/src/com/pigdogbay/weightrecorder/model/UnitConverterFactory.java
@@ -0,0 +1,152 @@
+package com.pigdogbay.weightrecorder.model;
+
+
+public class UnitConverterFactory
+{
+	/*
+	 * http://en.wikipedia.org/wiki/US_customary_units#Units_of_mass 
+	 * The pound avoirdupois, which forms the basis of the U.S. customary system of mass, 
+	 * is defined as exactly 453.59237 grams by agreement between the U.S., the U.K. 
+	 * and other English-speaking countries in 1959. 
+	 */
+	public static final double POUND_TO_KILOGRAM_FACTOR = 0.45359237d;
+	/*
+	 * http://en.wikipedia.org/wiki/Inch#Modern_standardisation 
+	 * The current internationally accepted value for the imperial and US customary inch is exactly 25.4 millimetres.
+	 */
+	public static final double INCH_METRE_FACTOR = 0.0254D;
+	
+	// Numbers must match arrays.xml
+	public final static int KILOGRAMS_TO_KILOGRAMS = 1;
+	public final static int KILOGRAMS_TO_POUNDS = 2;
+	public final static int KILOGRAMS_TO_STONES = 3;
+
+	public final static int METRES_TO_METRES = 1;
+	public final static int METRES_TO_CENTIMETRES = 2;
+	public final static int METRES_TO_INCHES = 3;
+
+	public static IUnitConverter create(int id)
+	{
+		switch (id)
+		{
+		case KILOGRAMS_TO_KILOGRAMS:
+			return new FactorConverter("kg", 1.0d);
+		case KILOGRAMS_TO_POUNDS:
+			return new FactorConverter("lb", 1d/POUND_TO_KILOGRAM_FACTOR,0.5d);
+		case KILOGRAMS_TO_STONES:
+			return new StonesConverter();
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static IUnitConverter createLengthConverter(int id)
+	{
+		switch (id)
+		{
+		case METRES_TO_METRES:
+			return new FactorConverter("m", 1.0d);
+		case METRES_TO_CENTIMETRES:
+			return new FactorConverter("cm", 100.0d);
+		case METRES_TO_INCHES:
+			return new FactorConverter("in", 1d/INCH_METRE_FACTOR);
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+}
+
+class FactorConverter implements IUnitConverter
+{
+	String units;
+	double factor;
+	double step=0.1d;
+	
+	public FactorConverter(String units, double factor)
+	{
+		this.units = units;
+		this.factor = factor;
+	}
+	public FactorConverter(String units, double factor, double step)
+	{
+		this.units = units;
+		this.factor = factor;
+		this.step = step;
+	}
+
+	public String getUnits()
+	{
+		return units;
+	}
+
+	public double convert(double value)
+	{
+		return value * factor;
+	}
+
+	public double inverse(double value)
+	{
+		return value / factor;
+	}
+
+	/**
+	 * NB Does not convert the weight
+	 * @param weight in the user units
+	 * @return string representation of the weight in user units
+	 */
+	public String getDisplayString(double weight)
+	{
+		return String.format("%.1f %s", weight, units);
+	}
+
+	public double getStepIncrement()
+	{
+		return step;
+	}
+}
+
+class StonesConverter implements IUnitConverter
+{
+	private static final String STONE_UNITS = "st";
+	private static final String POUND_UNITS = "lb";
+	
+	private static final double STONE_TO_POUND_FACTOR = 14d;
+	private static final double STONE_KILOGRAM = UnitConverterFactory.POUND_TO_KILOGRAM_FACTOR*STONE_TO_POUND_FACTOR;
+	public String getUnits()
+	{
+		return STONE_UNITS;
+	}
+
+	public double convert(double value)
+	{
+		return value/STONE_KILOGRAM;
+	}
+
+	public double inverse(double value)
+	{
+		return value*STONE_KILOGRAM;
+	}
+
+	/**
+	 * NB Does not convert the weight
+	 * @param weight in the user units
+	 * @return string representation of the weight in user units
+	 */
+	public String getDisplayString(double weight)
+	{
+		//Convert to pounds
+		weight = weight*STONE_TO_POUND_FACTOR;
+		int intWeight = (int)Math.round(weight);
+		int stones= intWeight/14;
+		int pounds=intWeight - stones*14;
+		return stones==0 ? 
+				String.format("%d %s", pounds, POUND_UNITS):
+				String.format("%d%s %d%s", stones, STONE_UNITS, pounds, POUND_UNITS);
+	}
+
+	public double getStepIncrement()
+	{
+		return 1.0d/14.0d;
+	}
+	
+}
