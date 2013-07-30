15518e82cab69399ac4f99582af2683f5f717fb9
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2013-07-30 14:32:17 +0000

    Removed payload check - not possible at the moment

--- trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ShopActivity.java
+++ trunk/WeightRecorder/src/com/pigdogbay/weightrecorder/ShopActivity.java
@@ -8,6 +8,7 @@ import com.pigdogbay.androidutils.iab.IabResult;
 import com.pigdogbay.androidutils.iab.Inventory;
 import com.pigdogbay.androidutils.iab.Purchase;
 import com.pigdogbay.androidutils.iab.SkuDetails;
+import com.pigdogbay.weightrecorder.model.AppPurchases;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -196,11 +197,8 @@ public class ShopActivity extends Activity {
     }
 
 	private void setUpIAB() {
-		// !SECURITY!
-		String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtj5nISgUocwBriIuzxABKLkNJmy1nKi5rj18gb/Cm0S9i6bNi4uo4+fu4XJKkBRYSgXScCIDVO+Tb9GCKupiUk7+o/xzxoOYYUiW22wbLU3fbTG5Wwctq2QyMnAgBDmDrn53k81vR0xBwWaqsIOm5GvKq46yNS4RWG+M7/vcw22f7MqpFpzFgWPxLPC42uU3u+C5+AInt5MXKHsaeoHDgrax1m2eyurnbI6sdJ3YseMQvjfJNHxbJjeXMBNR82Hzg17RIQIs+PayBjByu5Koc4depb9hwqf/c4PObumcemSpmHEjFJ6gbPyIMzPpKZGGqfG4RYZ8yE50+APLmt4kswIDAQAB";
-
 		// compute your public key and store it in base64EncodedPublicKey
-		_Helper = new IabHelper(this, base64EncodedPublicKey);
+		_Helper = new IabHelper(this, AppPurchases.getPublicKey());
 		// Start setup. This is asynchronous and the specified listener
 		// will be called once setup completes.
 		Log.d(TAG, "Starting setup.");
@@ -235,7 +233,7 @@ public class ShopActivity extends Activity {
         }};
             
 	private void buy(String sku) {
-		// !SECURITY!
+		// Ignore payload for now, need a server for it to be effective
 		String payload = "";
 		setWaitScreen(true);
 		_Helper.launchPurchaseFlow(this, sku, RC_REQUEST,
@@ -256,38 +254,6 @@ public class ShopActivity extends Activity {
             Log.d(TAG, "onActivityResult handled by IABUtil.");
         }
     }
-	/** Verifies the developer payload of a purchase. */
-	boolean verifyDeveloperPayload(Purchase p) {
-		String payload = p.getDeveloperPayload();
-
-		/*
-		 * TODO: verify that the developer payload of the purchase is correct.
-		 * It will be the same one that you sent when initiating the purchase.
-		 * 
-		 * WARNING: Locally generating a random string when starting a purchase
-		 * and verifying it here might seem like a good approach, but this will
-		 * fail in the case where the user purchases an item on one device and
-		 * then uses your app on a different device, because on the other device
-		 * you will not have access to the random string you originally
-		 * generated.
-		 * 
-		 * So a good developer payload has these characteristics:
-		 * 
-		 * 1. If two different users purchase an item, the payload is different
-		 * between them, so that one user's purchase can't be replayed to
-		 * another user.
-		 * 
-		 * 2. The payload must be such that you can verify it even when the app
-		 * wasn't the one who initiated the purchase flow (so that items
-		 * purchased by the user on one device work on other devices owned by
-		 * the user).
-		 * 
-		 * Using your own server to store and verify developer payloads across
-		 * app installations is recommended.
-		 */
-
-		return true;
-	}
 
 	// Callback for when a purchase is finished
 	IabHelper.OnIabPurchaseFinishedListener _PurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
@@ -300,12 +266,6 @@ public class ShopActivity extends Activity {
 						Toast.LENGTH_LONG).show();
 				return;
 			}
-			if (!verifyDeveloperPayload(purchase)) {
-				Toast.makeText(ShopActivity.this, "Unable to verify item.",
-						Toast.LENGTH_LONG).show();
-				return;
-			}
-
 			Log.d(TAG, "Purchase successful.");
 			refreshInventory();
 			if (purchase.getSku().equals(SKU_TEST)) {
