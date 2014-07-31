9e56aa039033ce03bd23ecbc96adf4e434804c28
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2014-07-31 15:57:56 +0000

    Instead of different layouts for each screen size, there are now different styles for the home buttons for each screen size.

--- WeightRecorder/src/com/pigdogbay/weightrecorder/MainActivity.java
+++ WeightRecorder/src/com/pigdogbay/weightrecorder/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends FragmentActivity implements OnSharedPreference
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_fragment);
+		setContentView(R.layout.activity_main);
 		PreferenceManager
 			.getDefaultSharedPreferences(this)
 			.registerOnSharedPreferenceChangeListener(this);
