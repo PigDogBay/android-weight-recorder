cdc93db642f9dd11bbdb08d31fa11f9082095096
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2014-08-07 11:23:30 +0000

    Release 1.14.03, 7th Aug 2014

---
+++ tags/v1_14_03/WeightRecorder/src/com/pigdogbay/weightrecorder/ReadingsChart.java
@@ -0,0 +1,139 @@
+package com.pigdogbay.weightrecorder;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+import com.pigdogbay.weightrecorder.model.ChartAxesRanges;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Paint.Align;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+public class ReadingsChart
+{
+	private static final float READINGS_LINE_WIDTH_DP = 2f;
+	private static final float TREND_LINE_WIDTH_DP = 1.8f;
+	private static final float LABELS_TEXT_SIZE_SP = 14f;
+	private static final float AXIS_TITLE_TEXT_SIZE_SP = 20f;
+	private static final int POINT_SIZE_DP = 3;
+	XYMultipleSeriesRenderer _Renderer;
+	XYMultipleSeriesDataset _Dataset;
+	Context _Context;
+	public ReadingsChart(Context context)
+	{
+		_Context = context;
+		_Renderer = new XYMultipleSeriesRenderer();
+		_Renderer.setShowLegend(false);
+		_Renderer.setPanEnabled(true);
+		_Renderer.setZoomEnabled(true);
+		_Renderer.setBackgroundColor(Color.BLACK);
+		_Renderer.setApplyBackgroundColor(true);
+		_Renderer.setAxesColor(Color.LTGRAY);
+		_Renderer.setLabelsColor(Color.LTGRAY);
+		_Renderer.setShowGrid(true);
+		_Renderer.setXLabelsAlign(Align.CENTER);
+		_Renderer.setYLabelsAlign(Align.RIGHT);
+		_Renderer.setZoomButtonsVisible(true);
+
+		_Dataset = new XYMultipleSeriesDataset();
+		
+	}
+	public void setXAxisTitle(String title)
+	{
+		_Renderer.setXTitle(title);
+	}
+
+	public void setYAxisTitle(String title)
+	{
+		_Renderer.setYTitle(title);
+	}
+	public void setChartAxesRanges(ChartAxesRanges chartAxesRanges)
+	{
+		_Renderer.setXAxisMin(chartAxesRanges.XAxisMin);
+		_Renderer.setXAxisMax(chartAxesRanges.XAxisMax);
+		_Renderer.setYAxisMin(chartAxesRanges.YAxisMin);
+		_Renderer.setYAxisMax(chartAxesRanges.YAxisMax);
+	}
+	
+	public void addReadings(TimeSeries series)
+	{
+		XYSeriesRenderer r = new XYSeriesRenderer();
+		r.setColor(Color.GREEN);
+		r.setPointStyle(PointStyle.CIRCLE);
+		r.setFillPoints(true);
+		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, READINGS_LINE_WIDTH_DP, _Context.getResources().getDisplayMetrics());
+		r.setLineWidth(size);
+		_Renderer.addSeriesRenderer(r);
+		_Dataset.addSeries(series);
+	}
+	public void addTrend(TimeSeries series)
+	{
+		XYSeriesRenderer r = new XYSeriesRenderer();
+		r.setColor(Color.RED);
+		r.setPointStyle(PointStyle.TRIANGLE);
+		r.setFillPoints(true);
+		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, TREND_LINE_WIDTH_DP, _Context.getResources().getDisplayMetrics());
+		r.setLineWidth(size);
+		_Renderer.addSeriesRenderer(r);
+		_Dataset.addSeries(series);
+	}
+	public void addTarget(TimeSeries series)
+	{
+		XYSeriesRenderer r = new XYSeriesRenderer();
+		r.setColor(Color.CYAN);
+		r.setPointStyle(PointStyle.SQUARE);
+		r.setFillPoints(true);
+		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, TREND_LINE_WIDTH_DP, _Context.getResources().getDisplayMetrics());
+		r.setLineWidth(size);
+		_Renderer.addSeriesRenderer(r);
+		_Dataset.addSeries(series);
+	}
+	private void fitDisplay()
+	{
+		DisplayMetrics metrics = _Context.getResources().getDisplayMetrics();
+		int screenSize = _Context.getResources().getConfiguration().screenLayout
+				& Configuration.SCREENLAYOUT_SIZE_MASK;
+		float size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, POINT_SIZE_DP, metrics);
+		_Renderer.setPointSize(size);
+		size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, AXIS_TITLE_TEXT_SIZE_SP, metrics);
+		_Renderer.setAxisTitleTextSize(size);
+		size = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, LABELS_TEXT_SIZE_SP, metrics);
+		_Renderer.setLabelsTextSize(size);
+		//Default large screen values
+		float marginLeft=48f,marginBottom=10f;
+		_Renderer.setXLabels(10);
+		_Renderer.setYLabels(10);
+		if (screenSize == Configuration.SCREENLAYOUT_SIZE_SMALL)
+		{
+			_Renderer.setXLabels(4);
+			_Renderer.setYLabels(4);
+			marginLeft = 30f;
+			marginBottom = 16f;
+		}
+		else if (screenSize == Configuration.SCREENLAYOUT_SIZE_NORMAL)
+		{
+			_Renderer.setXLabels(6);
+			_Renderer.setYLabels(6);
+			marginLeft = 44f;
+			marginBottom = 16f;
+		}
+		marginLeft = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, marginLeft, metrics);
+		marginBottom = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, marginBottom, metrics);
+		// Top, left, bottom, right
+		_Renderer.setMargins(new int[]{0,(int)marginLeft,(int)marginBottom,0});
+
+	}
+	public GraphicalView createView()
+	{
+		fitDisplay();
+		return ChartFactory.getTimeChartView(_Context, _Dataset, _Renderer,"d MMM");
+
+	}
+}
---
+++ tags/v1_14_03/WeightRecorder/src/com/pigdogbay/weightrecorder/model/ChartLogic.java
@@ -0,0 +1,162 @@
+package com.pigdogbay.weightrecorder.model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.achartengine.model.TimeSeries;
+
+public class ChartLogic {
+	
+	public static final double AXIS_DEFAULT_MIN_WEIGHT = 50D;
+	public static final double AXIS_DEFAULT_MAX_WEIGHT = 100D;
+	public static final double AXIS_MIN_PADDING = 0.5D;
+
+	public static final double AXIS_Y_PERCENTAGE_PADDING = 0.1D;
+	private static final long MILLIS_IN_DAY = 1000L*60L*60L*24L;
+	
+	UserSettings _UserSettings;
+	public ChartLogic(UserSettings userSettings) {
+		_UserSettings = userSettings;
+	}
+	/**
+	 * If period is 0,  the axes fit the entire data set. If period is 0 and 0 readings are passed, this is likely to
+	 * be a bug as readings should be guaranteed by callee, if this happens 0,0,0,0 is returned.
+	 * 
+	 * For period>0, the time axis, X, is set from period days past upto now.
+	 * If 0 readings are in this period then default y-axis values are used, this may happen if the user has not entered
+	 * any readings for a while.
+	 * 
+	 * The Y axis is padded by 10% of the min-max range both ends. The x-axis has one extra day added at the RHS
+	 * Minimum Y-axis padding is AXIS_MIN_PADDING
+	 * 
+	 * @param query containing data to plot
+	 * @param period in days
+	 */
+	public ChartAxesRanges calculateAxesRanges(Query query, long period) {
+		if (query.getReadings().size()==0)
+		{
+			return new ChartAxesRanges();
+		}
+		Date endTime = new Date();
+		Date startTime = new Date(endTime.getTime() - period * MILLIS_IN_DAY);
+		if (period == 0) {
+			startTime = query.getFirstReading().getDate();
+			endTime = query.getLatestReading().getDate();
+		}
+		else
+		{
+			query = query.getReadingsBetweenDates(startTime, endTime);
+		}
+		
+		ChartAxesRanges chartAxesRanges = calculateYAxisRange(query);
+		padYAxis(chartAxesRanges, AXIS_Y_PERCENTAGE_PADDING);
+		// add 1 day to current date
+		chartAxesRanges.XAxisMax = endTime.getTime() + MILLIS_IN_DAY;
+		chartAxesRanges.XAxisMin = startTime.getTime();
+		return chartAxesRanges;
+	}
+	/**
+	 * Calculate the Y Axis values based on the min and max values in the query
+	 * Values are converted into the user units
+	 * 
+	 * If 0 readings are in this period then default y-axis values are used, this may happen if the user has not entered
+	 * any readings for a while.
+	 * 
+	 * @param query
+	 * @return ChartAxesRanges with Y axis values set
+	 */
+	private ChartAxesRanges calculateYAxisRange(Query query)
+	{
+		ChartAxesRanges chartAxesRanges = new ChartAxesRanges();
+		double min = query.getMinWeight().getWeight();
+		double max = query.getMaxWeight().getWeight();
+		if (query.getReadings().size()==0)
+		{
+			min = AXIS_DEFAULT_MIN_WEIGHT;
+			max = AXIS_DEFAULT_MAX_WEIGHT;
+		}
+		chartAxesRanges.YAxisMin = _UserSettings.WeightConverter.convert(min);
+		chartAxesRanges.YAxisMax = _UserSettings.WeightConverter.convert(max);
+		
+		return chartAxesRanges;
+		
+	}
+	/*
+	 * Extends the Y-axis at both ends to give a more pleasing look
+	 */
+	private void padYAxis(ChartAxesRanges chartAxesRanges, double percentagePadding)
+	{
+		double padding = (chartAxesRanges.YAxisMax-chartAxesRanges.YAxisMin) *percentagePadding;
+		if (padding < AXIS_MIN_PADDING) {
+			padding = AXIS_MIN_PADDING;
+		}
+		chartAxesRanges.YAxisMin -= padding;
+		chartAxesRanges.YAxisMax += padding;
+	}	
+	
+	public TimeSeries createReadingsSeries(Query query)
+	{
+		TimeSeries series = new TimeSeries("weights");
+		for (Reading reading : query.getReadings())
+		{
+			double weight = reading.getWeight();
+			weight = _UserSettings.WeightConverter.convert(weight);
+			series.add(reading.getDate(), weight);
+		}
+		return series;
+	}
+	public TimeSeries createTargetSeries()
+	{
+		TimeSeries series = new TimeSeries("target");
+		//Display line one year back and forward
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.roll(Calendar.YEAR, false);
+		double targetWeight = _UserSettings.WeightConverter.convert(_UserSettings.TargetWeight);
+		for (int i=0;i<25; i++)
+		{
+			series.add(cal.getTime(), targetWeight);
+			cal.add(Calendar.MONTH,1);
+		}
+		return series;
+	}
+	public TimeSeries createTrendSeries(Query query, long period)
+	{
+		//ensure readings are sorted by date before proceding
+		query.sortByDate();
+		TimeSeries series = new TimeSeries("trend");
+		long startTime = new Date().getTime() - period * MILLIS_IN_DAY;
+		if (period==0)
+		{
+			//use first reading
+			startTime = query.getFirstReading().getDate().getTime();
+		}
+		else
+		{
+			query = query.getReadingsBetweenDates(new Date(startTime),new Date());
+		}
+		
+		if (query.getReadings().size()<3)
+		{
+			return series;
+		}
+		TrendAnalysis trendAnalysis = new TrendAnalysis(query.getReadings());
+
+		//Add start weight
+		double weight = trendAnalysis.getEstimatedWeight(startTime);
+		weight = _UserSettings.WeightConverter.convert(weight);
+		series.add(new Date(startTime), weight);
+		//Project forward 1 year
+		Calendar cal = Calendar.getInstance();
+		for (int i=0;i<13; i++)
+		{
+			weight = trendAnalysis.getEstimatedWeight(cal.getTimeInMillis());
+			weight = _UserSettings.WeightConverter.convert(weight);
+			series.add(cal.getTime(),weight);
+			cal.add(Calendar.MONTH,1);
+		}		
+		return series;
+	}
+	
+	
+}
