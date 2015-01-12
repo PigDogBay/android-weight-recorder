72d5954ce6c1fb0d279032b9c0c8dced2bf05355
markpdbailey@gmail.com <markpdbailey@gmail.com@492325ca-274e-cac5-4cc1-00cf0eb0af5f>
2015-01-12 09:45:50 +0000

    Removed analytics and play services

--- WeightTrackerPro/src/com/pigdogbay/weighttrackerpro/WeightRecorderApplication.java
+++ WeightTrackerPro/src/com/pigdogbay/weighttrackerpro/WeightRecorderApplication.java
@@ -2,38 +2,9 @@ package com.pigdogbay.weighttrackerpro;
 
 import java.util.List;
 
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-
-import android.app.Activity;
 import android.app.Application;
-import android.support.v4.app.Fragment;
 
 public class WeightRecorderApplication  extends Application{
-	private static final String CATEGORY = "Weight Recorder";
-	private static final String PREFIX = "WR ";
 	private List<String> _Dictionary;
-	private Tracker _Tracker;	
-
-	synchronized Tracker getTracker(){
-		if (_Tracker==null){
-			_Tracker = GoogleAnalytics.getInstance(this).newTracker(R.xml.app_tracker);
-		}
-		return _Tracker;
-	}
-	
-	public static void trackEvent(Activity activity, String screenName, String label){
-    	Tracker t = ((WeightRecorderApplication)activity.getApplication()).getTracker();
-    	t.send(new HitBuilders.EventBuilder()
-		.setCategory(CATEGORY)
-		.setAction("WR "+screenName)
-		.setLabel("WR "+label)
-		.build());
-	}	
-	
-	public static void trackEvent(Fragment fragment, String screenName, String label){
-		trackEvent(fragment.getActivity(), screenName, label);
-	}	
 
 }
